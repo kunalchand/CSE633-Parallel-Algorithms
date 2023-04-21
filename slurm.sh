23ad953e2f88f21cbb7c89eaa8637eef1c00d8d6
Kunal Chand <kunalchand234@gmail.com>
2023-04-21 02:00:01 -0400

    Working Slurm Script (via vortex)

--- slurm.sh
+++ slurm.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-#SBATCH --nodes=4
+#SBATCH --nodes=35
 #SBATCH --ntasks-per-node=1
 #SBATCH --constraint=IB|OPA
 #SBATCH --time=00:10:00
-#SBATCH --job-name="subsetsum-job"
-#SBATCH --output=../output/output.out
 #SBATCH --partition=general-compute
 #SBATCH --qos=general-compute
-#SBATCH --cluster=ub-hpc
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kchand@buffalo.edu
+#SBATCH --job-name="subsetsum-job-35"
+#SBATCH --output=../output/output-35.out
 #SBATCH --exclusive 
 
 module load intel
-# module list
+module list
 
-# export I_MPI_DEBUG=4
+export I_MPI_DEBUG=4
 export I_MPI_PMI_LIBRARY=/opt/software/slurm/lib64/libpmi.so
 
 mpicc -o ../classfile/subsetsum ../subsetsum.c
-mpirun -np 4 ../classfile/subsetsum
-# srun -n 4 ../classfile/subsetsum
+# mpirun -np 3535 ../classfile/subsetsum
+srun -n 35 ../classfile/subsetsum
