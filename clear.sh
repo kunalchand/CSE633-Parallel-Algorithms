16987620d032b370777c65ed341cb2aaf4c29157
Kunal Chand <kunalchand234@gmail.com>
2023-04-21 04:38:57 -0400

    added linear iteration

--- clear.sh
+++ clear.sh
@@ -4,6 +4,7 @@ clear
 
 for i in {0..8}; do
   task=$((2**i))
+  # task=$i
   rm ../output/output-$task.out
   rm ../output/error-output-$task.out
   sbatch --nodes=$task --job-name="subsetsum-job-$task" --output=../output/output-$task.out --error=../output/error-output-$task.out slurm.sh $task
