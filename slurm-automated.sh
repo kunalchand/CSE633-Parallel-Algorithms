662bea7b477c7e3e549e1378f67c64c753448e80
Kunal Chand <kunalchand234@gmail.com>
2023-04-22 03:03:22 -0400

    Create slurm-automated.sh

---
+++ slurm-automated.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --constraint=IB|OPA
+#SBATCH --time=00:10:00
+#SBATCH --partition=general-compute
+#SBATCH --qos=general-compute
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kchand@buffalo.edu
+#SBATCH --exclusive 
+
+module load intel
+# module list
+
+# export I_MPI_DEBUG=4
+export I_MPI_PMI_LIBRARY=/opt/software/slurm/lib64/libpmi.so
+
+# mpicc -o ../classfile/subsetsum ../subsetsum.c
+# mpirun -np $1 ../classfile/subsetsum
+srun -n $1 ../classfile/subsetsum
